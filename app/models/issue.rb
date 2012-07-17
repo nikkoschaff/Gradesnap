7ed1628a9a781309e8752270d36cd8ca235d39c6
nikko <nikko.schaff@gmail.com>
2012-07-17 13:15:24 -0700

    additions to issues, fixed styling problems, updated imgproc

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,13 +1,13 @@
 class Issue < ActiveRecord::Base
-  attr_accessible :code,
-		 :resolved,
-		 :row_id,
-		 :tablename,
-		 :teacher_id,
-		 :name
+  	attr_accessible :code,
+					:resolved,
+					:row_id,
+					:tablename,
+					:teacher_id,
+					:name
 
-	validates :code, :presence => true, :numericality => { :only_integer => true }
-	validates :resolved, :presence => true
+	validates :code, :presence => true
+	validates :resolved, :inclusion => {:in => [true, false]}
 	validates :row_id, :presence => true
 	validates :tablename, :presence => true
 	validates :teacher_id, :presence => true
