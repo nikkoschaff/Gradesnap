9aae1b5cc8b5cf0294e3b3cc2a5791621831bdc9
Conor Craig <conor@gradesnap.com>
2012-07-24 14:55:06 -0700

    Added validation for all 6 forms on site.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -40,7 +40,7 @@ class StudentsController < ApplicationController
   # GET /students/new.json
   def new
     @student = Student.new
-    @courses = Course.where("teacher_id=?", session[:user].teacher_id.to_a)
+    @courses = Course.where("teacher_id=?", session[:user].teacher_id)
 
     respond_to do |format|
       format.html # new.html.erb
