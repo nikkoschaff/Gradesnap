d4c2c61566013b9af9a6af1d1de90eca40caec35
Nikko Schaff <nikko.schaff@gmail.com>
2012-08-06 17:33:41 -0400

    minor security updates

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
  
   def welcome_email(user)
     @user = user
-    @url  = "http://gradesnap.com/prelogins/#{@user.id}/code"
+    @url  = "//gradesnap.com/prelogins/#{@user.id}/code"
     mail(:to => @user.email, :subject => "Welcome to Gradesnap")
   end
 
