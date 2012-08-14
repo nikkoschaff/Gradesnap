d395906a09b0605b4756901c2f552ede60da1d69
nikko <nikko.schaff@gmail.com>
2012-08-13 21:29:20 -0400

    General adjustments to static pages, pages commented where work is needed

--- db/migrate/20120806205142_change_string_to_text.rb
+++ db/migrate/20120806205142_change_string_to_text.rb
@@ -1,6 +1,5 @@
 class ChangeStringToText < ActiveRecord::Migration
   def up
-  	#remove_index(:assignment_students, [:student_id, :assignment_id])
     change_column :assignments, :answer_key, :text , :limit => nil
     change_column :assignments, :name, :text, :limit => nil
 	change_column :assignments, :answer_scansheet, :text, :limit => nil
@@ -13,8 +12,6 @@ class ChangeStringToText < ActiveRecord::Migration
 
 	change_column :assignment_students, :results, :text, :limit => nil
 	change_column :assignment_students, :answer_key, :text, :limit => nil
-	#add_index(:assignment_students, [:student_id, :assignment_id], :name => "add_index_to_assignment_students")
-	#change_column :assignment_students, :answer_key, :text, :limit => nil
   end
 
   def down
