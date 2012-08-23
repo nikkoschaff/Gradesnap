fa9a02e21b3248c53998bd978eaa176dbf9d61a2
nikko <nikko.schaff@gmail.com>
2012-08-22 21:54:29 -0400

    Fixed db migration issue (though it will require clean db after this commit)

--- db/migrate/20120806205142_change_string_to_text.rb
+++ db/migrate/20120806205142_change_string_to_text.rb
@@ -9,21 +9,8 @@ class ChangeStringToText < ActiveRecord::Migration
 	change_column :scansheets, :name, :text, :limit => nil
 	change_column :scansheets, :answers_string, :text, :limit => nil
 	change_column :scansheets, :assignment_student_id, :text, :limit => nil
-
-
-	#remove_index :assignments_students, :results
-	change_column :assignments_students, :results, :text, :limit => nil
-	#add_index :assignments_students, :results, :name => "results"
-
-	#remove_index :assignments_students, :answer_key
-	change_column :assignments_students, :answer_key, :text, :limit => nil
-	#add_index :assignments_students, :answer_key, :name => "answer_key"
-
-	#change_column :assignments_students, :results, :text, :limit => nil
-	#change_column :assignments_students, :answer_key, :text,  :limit => nil
-
   end
-
+ 
   def down
   	#remove_index(:assignment_students, :name => "add_index_to_assignment_students")
   end
