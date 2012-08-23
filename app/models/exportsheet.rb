0ff1a3eacd7c21f6762b8a48baac8f45a0385645
nikko <nikko.schaff@gmail.com>
2012-08-22 22:14:50 -0400

    Exportsheets now properly only outputs the student name and grades

--- app/models/exportsheet.rb
+++ app/models/exportsheet.rb
@@ -6,9 +6,10 @@ class Exportsheet < ActiveRecord::Base
 
   def self.to_csv(options = {})
     CSV.generate(options) do |csv_row|
-      csv_row << column_names
-      all.each do |product|
-        csv_row << product.attributes.values_at(*column_names)
+      csv_row << column_names[1..2]
+      all.each do |exportsheet|
+	nextrow = exportsheet.attributes.values_at(*column_names)[1..2]
+	csv_row << nextrow
       end
     end
   end
