9fbb7e932643583aabb1f3b9cf664255fc86efe5
Nikko Schaff <nikko.schaff@gmail.com>
2012-09-19 21:30:02 -0400

    commit to save before pull

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,4 +1,6 @@
 class Plan < ActiveRecord::Base
   has_many :users
   attr_accessible :name, :price
+  validates_uniqueness_of :name
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
 Plan.create!(:name => "Gradesnap Monthly (4.99/month)", :price => 4.99)
 Plan.create!(:name => "Gradesnap Yearly (49.99/year)", :price => 49.99)
 User.create!(:name => "Admin", :email => "admin@gradesnap.com", :hashed_password => "adminatgradesnap", :password => "adminatgradesnap", :plan_id => 1, :password_confirmation => "adminatgradesnap")
+User.create!(:name => "Guest", :email => "guest@gradesnap.com", :hashed_password => "guest1", :password => "guest1", :password_confirmation => "guest1", :plan_id => 1, :teacher_id => 1)
