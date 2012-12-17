f940804a06f1d7b245c8fa15c1b4cce265dabff0
nikko <nikko.schaff@gmail.com>
2012-12-17 14:29:12 -0500

    Added masterStyles to precompliation setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Gradesnap::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-   config.assets.precompile += %w( jquery-fileupload/* rails.validations* )
+   config.assets.precompile += %w( jquery-fileupload/* rails.validations* masterStyles* )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
