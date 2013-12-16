d8ec8163341f9f8dca5328256512bce0da1e16f1
Nikko Schaff <nikko.schaff@gmail.com>
2013-12-16 13:06:17 +0000

    force for production

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-#ENV['RAILS_ENV'] ||= 'production'
+ENV['RAILS_ENV'] ||= 'production'
 
 RCC_PUB='6LeLcdQSAAAAAFHtgu3ldiPuKoAP0Ab87Er-mNyJ' #normal constant decl
 RCC_PRIV = '6LeLcdQSAAAAALw_QJHD7BdCaWTo6L08HWVcaWxl'
